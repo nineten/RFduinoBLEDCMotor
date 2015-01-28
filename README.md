5f8bc143a381c3c53f5573116b1a6bb0e8882c26
nineten <jayden87@gmail.com>
2015-01-28 11:37:42 +0800

    Cloned and cleaned from RFduinoBLEServo

---
+++ README.md
@@ -0,0 +1,9 @@
+# RFduinoBLEDCMotor
+Example code to control a DC motor with varying speed using an RFduino through BLE
+
+
+## Setup
+#### RFduino
+- [RFduino](https://github.com/RFduino/RFduino)
+- Follow the setup instructions on the site
+
---
+++ RFduinoBLEDCMotor.ino
@@ -0,0 +1,69 @@
+#include <RFduinoBLE.h>
+
+// interval between advertisement transmissions ms (range is 20ms to 10.24s) - default 20ms
+int interval = 500;  // 675 ms between advertisement transmissions
+int pos = 0;
+
+void setup() {
+  // led used to indicate that the RFduino is advertising
+	Serial.begin(9600);
+
+  // this is the data we want to appear in the advertisement
+  // (if the deviceName and advertisementData are too long to fix into the 31 byte
+  // ble advertisement packet, then the advertisementData is truncated first down to
+  // a single byte, then it will truncate the deviceName)
+  RFduinoBLE.advertisementData = "ledbtn";
+  RFduinoBLE.advertisementInterval = interval;
+  RFduinoBLE.deviceName = "JBLEServo";
+  RFduinoBLE.txPowerLevel = -20;
+
+  // change the advertisement interval
+  RFduinoBLE.advertisementInterval = interval;
+
+  // start the BLE stack
+  RFduinoBLE.begin();
+}
+
+void loop() {
+	// switch to lower power mode
+	RFduino_ULPDelay(INFINITE);
+}
+
+void RFduinoBLE_onAdvertisement(bool start)
+{
+  if (start) {
+		Serial.println("RFduino BLE advertising started");
+	} else {
+		Serial.println("RFduino BLE advertising stopped");
+	}
+}
+
+void RFduinoBLE_onConnect()
+{
+	Serial.println("RFduino BLE connection successful");
+}
+
+void RFduinoBLE_onDisconnect()
+{
+	Serial.println("RFduino BLE disconnected");
+}
+
+void RFduinoBLE_onReceive(char *data, int len)
+{
+	// If the first byte is 0x01 / on / true
+	Serial.println("Received data over BLE");
+	Serial.println(int(data[0]));
+	if (data[0])
+	{
+		ToggleMotor(int(data[0]));
+		Serial.println("Toggle motor");
+	}
+	else {
+		Serial.println("Do Nothing");
+	}
+}
+
+void ToggleMotor(int val) {
+	// Change speed accordingly
+	Serial.println(val);
+}
